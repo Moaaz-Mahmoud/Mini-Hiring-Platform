e0a555817f5471a53b51ed2363f6174877731dcf
Moaaz Mahmoud <fasekhs@gmail.com>
2022-05-12 10:56:05 +0200

    Meow

--- src/frontend/home.js
+++
@@ -1,9 +0,0 @@
-function redirectSignin(){
-    window.location.href = "sign-in.html"
-}
-function redirectSignup(){
-    window.location.href = "sign-up.html"
-}
-function redirectAbout(){
-    window.location.href = "about.html"
-}
